c68cac7ecb1a5940df99452842e4a6b406ad10fd
374402991@qq.com <zwl374402991>
2019-08-26 17:45:18 +0800

    first commit

---
+++ src/main/java/com/awsapi/AwsapiApplication.java
@@ -0,0 +1,13 @@
+package com.awsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/awsapi/AwsapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.awsapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsapiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
